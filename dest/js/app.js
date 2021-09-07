ee20564358653785ba9cf355b734e17344ac6705
Andrii <Luckyhidalgo@gmail.com>
2021-09-07 13:42:50 +0300

    Gallery page :::  - created beforeAfter section - w\o adaptive;

--- dest/js/app.js
+++ dest/js/app.js
@@ -20,46 +20,68 @@
  */
 var initPreventBehavior = function initPreventBehavior() {
 
-	var link = document.querySelectorAll("a");
+  var link = document.querySelectorAll("a");
 
-	link.forEach(function (val, idx) {
+  link.forEach(function (val, idx) {
 
-		val.addEventListener("click", function (e) {
-			if (val.getAttribute("href") === "#") {
-				e.preventDefault();
-			}
-		});
-	});
+    val.addEventListener("click", function (e) {
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
+};
+
+/**
+ * @name initSwiper
+ *
+ * @description initialize Swiper
+ */
+var initSwiper = function initSwiper() {
+
+  for (var i = 1; i <= $('.beforeAfter__slider').length; i++) {
+    var speed = 4000;
+
+    new Swiper('.beforeAfterSlider' + i, {
+      effect: 'slide',
+      speed: speed + i * 1000,
+      autoplay: {
+        delay: 5000
+      },
+      slidesPerView: 'auto',
+      spaceBetween: 15
+    });
+  }
 };
 
 /**
  * @description Document DOM ready.
  */
 (function () {
-	/*
- * =============================================
- * CALLBACK :: start
- * ============================================= */
+  /*
+  * CALLBACK :: start
+  * ============================================= */
 
-	/*
- * CALLBACK :: end
- * ============================================= */
+  /*
+  * CALLBACK :: end
+  * ============================================= */
 
-	/**
-  * @name initNative
-  *
-  * @description Init all method
-  */
-	var initNative = function initNative() {
-		// default
-		initPreventBehavior();
-		// ==========================================
+  /**
+   * @name initNative
+   *
+   * @description Init all method
+   */
+  var initNative = function initNative() {
+    // default
+    initPreventBehavior();
+    // ==========================================
 
-		// lib
-		// ==========================================
+    // lib
+    initSwiper();
+    // ==========================================
 
-		// callback
-		// ==========================================
-	};
-	initNative();
+    // callback
+    // ==========================================
+  };
+  initNative();
 })();
---
+++ src/js/_lib/swiper.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * @name initSwiper
+ *
+ * @description initialize Swiper
+ */
+const initSwiper = () => {
+
+  for(let i = 1; i <= $('.beforeAfter__slider').length; i++) {
+    let speed = 4000;
+    
+    new Swiper('.beforeAfterSlider' + i, {
+      effect: 'slide',
+      speed: speed + (i * 1000),
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 'auto',
+      spaceBetween: 15
+    });
+  }
+};
